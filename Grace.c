6deebaa20a1d153bffbe2673a6c47f6b38595584
Julien DEBLADIS <jdebladi@e3r6p18.42.fr>
2017-12-24 16:57:33 +0100

    finished Sully, added makefile

--- Grace.c
+++ Grace.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#define not_a_main main
 #define terminate return
 #define DQ '"'
+#define not_a_main int main() {void *kid = fopen("./Grace_kid.c", "w");char *s = "#include <stdio.h>%c#define terminate return%c#define DQ %c%c%c%c#define not_a_main int main() {void *kid = fopen(%c./Grace_kid.c%c, %cw%c);char *s = %c%s%c;fprintf(kid, s, 10, 10, 39, DQ, 39, 10, DQ, DQ, DQ, DQ, DQ, s, DQ, 10, 10, 47, 42, 10, 10, 42, 47, 10, 10);terminate 0;}%c%c%c%c%c    This is yet another comment%c%c%c%c%cnot_a_main";fprintf(kid, s, 10, 10, 39, DQ, 39, 10, DQ, DQ, DQ, DQ, DQ, s, DQ, 10, 10, 47, 42, 10, 10, 42, 47, 10, 10);terminate 0;}
 
-// This is yet another comment
+/*
+    This is yet another comment
+*/
 
-int not_a_main() {
-    void *kid = fopen("./Grace_kid.c", "w");
-    char *s = "#include <stdio.h>%c#define not_a_main main%c#define terminate return%c#define DQ %c%c%c%c%c%c%c This is yet another comment%c%cint not_a_main() {%c    void *kid = fopen(%c./Grace_kid.c%c, %cw%c);%c    char *s = %c%s%c;%c    fprintf(kid, s, 10, 10, 10, 39, DQ, 39, 10, 10, 47, 47, 10, 10, 10, DQ, DQ, DQ, DQ, 10, DQ, s, DQ, 10, 10, 10, 10);%c    terminate 0;%c}";
-    fprintf(kid, s, 10, 10, 10, 39, DQ, 39, 10, 10, 47, 47, 10, 10, 10, DQ, DQ, DQ, DQ, 10, DQ, s, DQ, 10, 10, 10, 10);
-    terminate 0;
-}
+not_a_main
---
+++ Makefile
@@ -0,0 +1,43 @@
+define \n
+
+
+endef
+SRC = Colleen.c Grace.c Sully.Colleen
+OBJ = $(SRC:.c=.o)
+NAME = $(SRC:.c=)
+CFLAGS = -Wall -Wextra -Werror
+
+all:
+	@$(MAKE) Colleen
+	@$(MAKE) Grace
+	@$(MAKE) Sully
+
+test:
+	@echo "\033[34mTesting Colleen \033[33m output checkable in Colleen_diff\033[0m"
+	@./Colleen > Colleen_diff
+	diff Colleen.c Colleen_diff
+	@echo ${\n}
+	@echo "\033[34mTesting Grace\033[0m"
+	@./Grace
+	diff Grace_kid.c Grace.c
+	@echo ${\n}
+	@echo "\033[34mTesting Sully \033[33m plus multiple diffs with Sully.c and a final grep, enjoy\033[0m"
+	@./Sully
+	for number in {5..0} ; do diff Sully.c Sully_$$number.c ||: ; done
+	@echo ${\n}
+	ls -la | grep Sully | wc -l
+
+clean:
+	@/bin/rm -f $(OBJ)
+	@echo "\033[31mClean \033[32mDone\033[0m"
+
+fclean: clean
+	@/bin/rm -f Sully Colleen Colleen_diff Grace Grace_kid.c Sully_0.c Sully_1 Sully_1.c Sully_2 Sully_2.c Sully_3 Sully_3.c Sully_4 Sully_4.c Sully_5 Sully_5.c
+	@echo "\033[31mFclean\033[32m Done\033[0m"
+
+re:
+	@$(MAKE) fclean
+	@$(MAKE) all
+	@echo "\033[34mRe\033[33mDone\033[0m"
+
+.PHONY: all clean fclean re test
